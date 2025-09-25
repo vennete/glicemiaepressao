09b7b186c9f88be3bfe5a9a24d8d385dcd9d0501
vennete <paulo.projetosc@gmail.com>
2025-09-25 09:19:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def registrar():
     params = request.args.to_dict()
     params["ip"] = request.headers.get("X-Forwarded-For", request.remote_addr)
 
-    url = "https://script.google.com/macros/s/SEU_SCRIPT_ID/exec"
+    url = "https://script.google.com/macros/s/AKfycbwcmuQ8ewoMlM9jl4pUIltGxTN6cLcLcJjYLH3bFef8FMHVaFRGKA_vUDMGLKF6GhOnWA/exec"
     r = requests.get(url, params=params)
 
     return r.text
